694faf8a3f4e79da7a55f975a9fc482019f3c52d
sihame123 <sihamelmorid@gmail.com>
2023-07-20 19:35:29 -0700

    C - Debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* main_h */
