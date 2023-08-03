5f5ca72d878e9a1286660f66f10b9576178dfef2
sihame123 <sihamelmorid@gmail.com>
2023-08-02 17:58:42 -0700

    task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,6 +2,8 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power
  * Return: result of the power
  */
 int _pow_recursion(int x, int y)
